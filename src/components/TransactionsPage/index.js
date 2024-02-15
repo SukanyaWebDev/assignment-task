3885bfc797b47403aeed40ee4c440d147d56c67d
sukanyawebdev <sukanyat211@gmail.com>
2024-02-15 16:05:22 +0530

    solution 3

--- src/components/TransactionsPage/index.js
+++ src/components/TransactionsPage/index.js
@@ -4,13 +4,25 @@ import './index.css'
 const TransactionsPage = () => (
     <div className="transaction-container">
         <div className="trans-header"><h1>Transactions</h1>
-        <ul className="list-of-con-trans">
-            <li><TiHomeOutline /></li>
-            <li> / General /</li>
-            <li>/ Transactions</li>
+            <ul className="list-of-con-trans">
+                <li><TiHomeOutline /> /</li>
+                <li>General/</li>
+                <li>Transactions</li>
             </ul>
         </div>
-        <div className="disply-trans-containe"></div>
+        
+        <div className="disply-trans-containe">
+        <hr/>
+            <ul className="transctions-list">
+                <li>id</li>
+                <li>Name</li>
+                <li>Amount</li>
+                <li>Transaction id</li>
+                <li>Date</li>
+                <li>Pancard</li>
+            </ul>
+            <hr/>
+        </div>
 
     </div>
 )
